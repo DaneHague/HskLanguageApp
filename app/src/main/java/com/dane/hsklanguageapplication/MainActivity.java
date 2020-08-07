8dfd8150710a8bf11191b0c68feb52bec207b6a1
DaneHague <danehague1@googlemail.com>
2020-08-07 14:11:24 +0100

    first push

---
+++ app/src/main/java/com/dane/hsklanguageapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dane.hsklanguageapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
