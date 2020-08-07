cf2145346ae7aa4062258562ee9980da062336e8
DaneHague <danehague1@googlemail.com>
2020-08-07 16:56:16 +0100

    first Commit, implemented hsk1-6 hanzi choice game

---
+++ app/src/main/java/com/dane/hsklanguageapplication/HskHanziChoiceGame.java
@@ -0,0 +1,283 @@
+package com.dane.hsklanguageapplication;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class HskHanziChoiceGame extends AppCompatActivity {
+    private TextView textView;
+    private Button answerBtn1;
+    private Button answerBtn2;
+    private Button answerBtn3;
+    private Button answerBtn4;
+    private Button btnNext;
+    private TextView txtTranslation;
+    private ImageButton btnFlipPinyin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hsk_hanzi_choice_game);
+
+        Intent intent = getIntent();
+        String hsk = intent.getStringExtra("hsk");
+
+        Integer score = new Integer(0);
+        createGame(score, true, hsk);
+    }
+
+    public String readJSONFromAsset(String hsk) {
+        String json = null;
+        try {
+            InputStream is = getAssets().open(hsk);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+
+    public JSONArray selectHanZi(String hsk) throws JSONException {
+        JSONArray jsA = new JSONArray(readJSONFromAsset(hsk));
+        JSONArray selectedHanZi = new JSONArray();
+
+        for (int i = 0; i < 4; i++){
+            JSONObject tmp = new JSONObject();
+
+            double randomHanZi = Math.random() * ((jsA.length() - 1)) + 0;
+            tmp.put("hanzi",jsA.getJSONObject((int) (randomHanZi)).optString("hanzi"));
+            tmp.put("pinyin", jsA.getJSONObject((int) (randomHanZi)).optString("pinyin"));
+            tmp.put( "translations", jsA.getJSONObject((int) (randomHanZi)).optString("translations"));
+            selectedHanZi.put(i, tmp);
+            jsA.remove((int) randomHanZi);
+        }
+        return selectedHanZi ;
+    }
+
+    public void createGame(final int score, boolean hanzi, String hsk){
+        final Integer[] countingScore = {score};
+        final TextView textScore = (TextView) findViewById(R.id.txtScore);
+        txtTranslation = findViewById(R.id.txtTranslation);
+        btnFlipPinyin = findViewById(R.id.btnFlipPinyin);
+        textScore.setText("Score: " + score);
+        textView = (TextView) findViewById(R.id.txt1);
+        final Button[] allButtons;
+        answerBtn1 = (Button) findViewById(R.id.btnAnswer1);
+        answerBtn2 = (Button) findViewById(R.id.btnAnswer2);
+        answerBtn3 = (Button) findViewById(R.id.btnAnswer3);
+        answerBtn4 = (Button) findViewById(R.id.btnAnswer4);
+        allButtons = new Button[]{answerBtn1, answerBtn2, answerBtn3, answerBtn4};
+        answerBtn1.setBackgroundColor(Color.parseColor("#ffffff"));
+        answerBtn2.setBackgroundColor(Color.parseColor("#ffffff"));
+        answerBtn3.setBackgroundColor(Color.parseColor("#ffffff"));
+        answerBtn4.setBackgroundColor(Color.parseColor("#ffffff"));
+        btnNext = findViewById(R.id.btnNext);
+        btnNext.setEnabled(false);
+        allButtons[0].setEnabled(true);
+        allButtons[1].setEnabled(true);
+        allButtons[2].setEnabled(true);
+        allButtons[3].setEnabled(true);
+        txtTranslation.setText("");
+        btnNext.setVisibility(textView.INVISIBLE);
+        btnNext.setBackgroundColor(Color.parseColor("#ffffff"));
+        final Boolean[] setHanzi = {hanzi};
+
+
+        try {
+            final JSONArray selectedCharacters = selectHanZi(hsk);
+            //Display the selected hanzi
+            //We have already randomized everything so we can simply set the answers and fake answers like so
+            final String answerPinyin = selectedCharacters.getJSONObject((int) (0)).optString("pinyin");
+            final String answerHanzi = selectedCharacters.getJSONObject((int) (0)).optString("hanzi");
+            if(setHanzi[0] == true){
+                textView.setText(selectedCharacters.getJSONObject((int) (0)).optString("hanzi"));
+            } else {
+                textView.setText(selectedCharacters.getJSONObject((int) (0)).optString("pinyin"));
+            }
+
+            final String translation = selectedCharacters.getJSONObject((int) (0)).optString("translations").replaceAll("\\[|]|\"","").replace(",", ", ");
+            Collections.shuffle(Arrays.asList(allButtons));
+
+            for(int i = 0; i < 4; i++){
+                if(setHanzi[0] == true){
+                    allButtons[i].setText(selectedCharacters.getJSONObject((int) (i)).optString("pinyin"));
+                } else {
+                    allButtons[i].setText(selectedCharacters.getJSONObject((int) (i)).optString("hanzi"));
+                }
+
+            }
+
+            btnFlipPinyin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    try {
+
+                        flipIt(allButtons[0]);
+                        flipIt(allButtons[1]);
+                        flipIt(allButtons[2]);
+                        flipIt(allButtons[3]);
+
+                        if (setHanzi[0] == true){
+                            setHanzi[0] = false;
+                        } else  {
+                            setHanzi[0] = true;
+                        }
+
+                        if (setHanzi[0] == true){
+                            textView.setText(selectedCharacters.getJSONObject((int) (0)).optString("hanzi"));
+                            for(int i = 0; i < 4; i++){
+                                allButtons[i].setText(selectedCharacters.getJSONObject((int) (i)).optString("pinyin"));
+                            }
+
+                        } else {
+
+                            textView.setText(selectedCharacters.getJSONObject((int) (0)).optString("pinyin"));
+                            for(int i = 0; i < 4; i++){
+                                allButtons[i].setText(selectedCharacters.getJSONObject((int) (i)).optString("hanzi"));
+                            }
+                        }
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+
+            allButtons[0].setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v){
+                    if (allButtons[0].getText() == answerPinyin || allButtons[0].getText() == answerHanzi){
+                        countingScore[0] += 1;
+                        allButtons[0].setBackgroundColor(Color.parseColor("green"));
+                        textScore.setText("Score: " + countingScore[0]);
+                        btnNext.setEnabled(true);
+                        allButtons[0].setEnabled(false);
+                        allButtons[1].setEnabled(false);
+                        allButtons[2].setEnabled(false);
+                        allButtons[3].setEnabled(false);
+                        txtTranslation.setText(translation);
+                        btnNext.setVisibility(textView.VISIBLE);
+                    }
+                    else {
+                        allButtons[0].setText("Try again!");
+                        allButtons[0].setBackgroundColor(Color.parseColor("red"));
+                        countingScore[0] =0;
+                        textScore.setText("Score: " + 0);
+                    }
+                }
+            });
+
+            allButtons[1].setOnClickListener(new View.OnClickListener(){
+                @RequiresApi(api = Build.VERSION_CODES.O)
+                public void onClick(View v){
+                    if (allButtons[1].getText() == answerPinyin || allButtons[1].getText() == answerHanzi){
+                        countingScore[0] += 1;
+                        allButtons[1].setBackgroundColor(Color.parseColor("green"));
+                        textScore.setText("Score: " + countingScore[0]);
+                        btnNext.setEnabled(true);
+                        allButtons[0].setEnabled(false);
+                        allButtons[1].setEnabled(false);
+                        allButtons[2].setEnabled(false);
+                        allButtons[3].setEnabled(false);
+                        txtTranslation.setText(translation);
+                        btnNext.setVisibility(textView.VISIBLE);
+                    }
+                    else {
+                        allButtons[1].setText("Try again!");
+                        allButtons[1].setBackgroundColor(Color.parseColor("red"));
+                        countingScore[0] =0;
+                        textScore.setText("Score: " + 0);
+                    }
+                }
+            });
+
+            allButtons[2].setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v){
+                    if (allButtons[2].getText() == answerPinyin || allButtons[2].getText() == answerHanzi){
+                        countingScore[0] += 1;
+                        allButtons[2].setBackgroundColor(Color.parseColor("green"));
+                        textScore.setText("Score: " + countingScore[0]);
+                        btnNext.setEnabled(true);
+                        allButtons[0].setEnabled(false);
+                        allButtons[1].setEnabled(false);
+                        allButtons[2].setEnabled(false);
+                        allButtons[3].setEnabled(false);
+                        txtTranslation.setText(translation);
+                        btnNext.setVisibility(textView.VISIBLE);
+                    }
+                    else {
+                        allButtons[2].setText("Try again!");
+                        allButtons[2].setBackgroundColor(Color.parseColor("red"));
+                        countingScore[0] =0;
+                        textScore.setText("Score: " + 0);
+                    }
+                }
+            });
+
+            allButtons[3].setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v){
+                    if (allButtons[3].getText() == answerPinyin || allButtons[3].getText() == answerHanzi){
+                        countingScore[0] += 1;
+                        allButtons[3].setBackgroundColor(Color.parseColor("green"));
+                        textScore.setText("Score: " + countingScore[0]);
+                        btnNext.setEnabled(true);
+                        allButtons[0].setEnabled(false);
+                        allButtons[1].setEnabled(false);
+                        allButtons[2].setEnabled(false);
+                        allButtons[3].setEnabled(false);
+                        txtTranslation.setText(translation);
+                        btnNext.setVisibility(textView.VISIBLE);
+                    }
+                    else {
+                        allButtons[3].setText("Try again!");
+                        allButtons[3].setBackgroundColor(Color.parseColor("red"));
+                        countingScore[0] =0;
+                        textScore.setText("Score: " + 0);
+                    }
+                }
+            });
+
+            btnNext.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = getIntent();
+                    String hsk = intent.getStringExtra("hsk");
+                    createGame(countingScore[0], setHanzi[0], hsk);
+                }
+            });
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+    private void flipIt(final View viewToFlip) {
+        ObjectAnimator flip = ObjectAnimator.ofFloat(viewToFlip, "rotationX", 0f, 360f);
+        flip.setDuration(600);
+        flip.start();
+    }
+}
--- app/src/main/java/com/dane/hsklanguageapplication/MainActivity.java
+++ app/src/main/java/com/dane/hsklanguageapplication/MainActivity.java
@@ -2,7 +2,10 @@ package com.dane.hsklanguageapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,91 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button btnHsk1 = findViewById(R.id.hsk1);
+        Button btnHsk2 = findViewById(R.id.hsk2);
+        Button btnHsk3 = findViewById(R.id.hsk3);
+        Button btnHsk4 = findViewById(R.id.hsk4);
+        Button btnHsk5 = findViewById(R.id.hsk5);
+        Button btnHsk6 = findViewById(R.id.hsk6);
+
+        btnHsk1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHsk1();
+            }
+        });
+
+        btnHsk2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHsk2();
+            }
+        });
+
+        btnHsk3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHsk3();
+            }
+        });
+
+        btnHsk4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHsk4();
+            }
+        });
+
+        btnHsk5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHsk5();
+            }
+        });
+
+        btnHsk6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHsk6();
+            }
+        });
+    }
+
+    public void openHsk1 (){
+        Intent openHsk1Intent = new Intent(this, HskHanziChoiceGame.class);
+        openHsk1Intent.putExtra("hsk", "hsk1.json");
+        startActivity(openHsk1Intent);
+    }
+
+    public void openHsk2 (){
+        Intent openHsk2Intent = new Intent(this, HskHanziChoiceGame.class);
+        openHsk2Intent.putExtra("hsk", "hsk2.json");
+        startActivity(openHsk2Intent);
+    }
+
+    public void openHsk3 (){
+        Intent openHsk3Intent = new Intent(this, HskHanziChoiceGame.class);
+        openHsk3Intent.putExtra("hsk", "hsk3.json");
+        startActivity(openHsk3Intent);
+    }
+
+    public void openHsk4 (){
+        Intent openHsk4Intent = new Intent(this, HskHanziChoiceGame.class);
+        openHsk4Intent.putExtra("hsk", "hsk4.json");
+        startActivity(openHsk4Intent);
+    }
+
+    public void openHsk5 (){
+        Intent openHsk5Intent = new Intent(this, HskHanziChoiceGame.class);
+        openHsk5Intent.putExtra("hsk", "hsk5.json");
+        startActivity(openHsk5Intent);
+    }
+
+    public void openHsk6 (){
+        Intent openHsk6Intent = new Intent(this, HskHanziChoiceGame.class);
+        openHsk6Intent.putExtra("hsk", "hsk6.json");
+        startActivity(openHsk6Intent);
     }
 }
